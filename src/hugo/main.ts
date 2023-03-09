3a52c6c91d02aeec63194e065e65d03747a77cd5
Hugo <hugoattal@hotmail.fr>
2023-03-09 20:04:58 +0100

    Add db connect

---
+++ src/hugo/main.ts
@@ -0,0 +1,11 @@
+import * as mongoose from "mongoose";
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/wateryuka");
+}
+
+
+main().catch(err => console.log(err));
+
+
+
